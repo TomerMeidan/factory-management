3e43d2305031ad4dcf47394028458f20f2930c03
Tomer Meidan <tomer.m@circ.zone>
2023-10-16 16:01:07 +0300

    added auth libs

--- server-side/utils/logger.js
+++ server-side/utils/logger.js
@@ -1,14 +1,26 @@
 // logger.js
 
 const fs = require("fs");
+const jsonFile = require("jsonfile")
 const logFile = fs.createWriteStream("./app.log", { flags: "w" });
+const actionFile = fs.createWriteStream("./actions.json", { flags: "w" });
 
 const originalLog = console.log;
 
 console.log = (args) => {
-  const time = new Date().toISOString();
-  originalLog(`[${time}]: ${args}`);
-  logFile.write(`[${time}]: ${args}\n`);
+  const time = new Date();
+  originalLog(
+    `[${time.toLocaleDateString()} ${time.toLocaleTimeString()}]: ${args}`
+  );
+  logFile.write(
+    `[${time.toLocaleDateString()} ${time.toLocaleTimeString()}]: ${args}\n`
+  );
+};
+
+actionUpdate = (args) => {
+  actionFile.write(
+    `[${time.toLocaleDateString()} ${time.toLocaleTimeString()}]: ${args}\n`
+  );
 };
 
 process.on("exit", () => {
